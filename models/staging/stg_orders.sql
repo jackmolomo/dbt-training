9ee1e84d3d5d9e56805cd30178266ca2c13251a0
jackmolomo@gmail.com <jackmolomo@gmail.com>
2022-09-12 14:42:23 +0000

    developing stg_orders

---
+++ models/staging/stg_orders.sql
@@ -0,0 +1,21 @@
+select 
+o.orderid,
+o.ORDERDATE,
+o.SHIPDATE,
+o.SHIPMODE,
+o.ORDERCOSTPRICE- ORDERSELLINGPRICE as oderprofit,
+o.ORDERCOSTPRICE,
+o.ORDERSELLINGPRICE,
+c.CUSTOMERNAME,
+c.SEGMENT,
+c.COUNTRY,
+p.CATEGORY,
+p.PRODUCTNAME,
+p.SUBCATEGORY
+
+from {{ ref('raw_orders') }} as o
+left join {{ ref('raw_customer') }} as c
+on o.CUSTOMERID = c.CUSTOMERID
+
+left join  {{ ref('raw_product') }} as p
+on o.PRODUCTID = p.PRODUCTID
